f24a24486d39923dd5886a735a2fe0e65905d16f
AkramovDiyor <akramovakramov32@gmail.com>
2026-09-25 12:43:48 +0300

    ok

--- client/src/api/stopList.api.ts
+++ client/src/api/stopList.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://stop-list-server.onrender.com/api',
 });
 
 export type DishCategory = 'Кухня' | 'Бар' | 'Десерты';
